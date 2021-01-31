5cdf9ff646a0af8bd78607efe9f9114df530ea9e
dongwook <dongwook224@gmail.com>
2021-02-01 06:45:03 +0900

    set react development environment

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { SourceMapDevToolPlugin } = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    devtool: false,
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/transform-runtime"],
+                },
+            },
+            {
+                test: /\.(scss|css)$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    output: {
+        publicPath: "",
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./public"),
+        port: 3000,
+        hot: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./public/index.html" }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new SourceMapDevToolPlugin(),
+        new webpack.BannerPlugin({ banner: new Date().toLocaleString() }),
+        new ESLintPlugin(),
+    ],
+};
